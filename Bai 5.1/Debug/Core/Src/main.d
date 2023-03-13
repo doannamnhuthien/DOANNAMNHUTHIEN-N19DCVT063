eb5bea63e605905368c014a497ff0b59359a014f
doannamnhuthien <nhuthien1122@gmail.com>
2023-03-13 22:44:18 +0700

    first commit

---
+++ Bai 5.1/Debug/Core/Src/main.d
@@ -0,0 +1,17 @@
+Core/Src/main.o: ../Core/Src/main.c \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
---
+++ Bai 5.1/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.d
@@ -0,0 +1,52 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Core/Inc/stm32f4xx_hal_conf.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/THIEN/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/THIEN/STM32CubeIDE/workspace_1.11.2/Bai\ 5.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
